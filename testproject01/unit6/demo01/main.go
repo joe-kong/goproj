6166a540a2f3e7e7b7621773393d1e76c8c847d5
reijun kou <kou_reijun@i3-medical.co.jp>
2024-01-05 01:03:49 +0900

    no message

--- testproject01/unit6/demo01/main.go
+++ testproject01/unit6/demo01/main.go
@@ -6,24 +6,24 @@ import "fmt"
 
 //错误处理和捕获机制
 
-//go语言吹球代码优雅，引入了defer+recover机制来处理错误
+//go语言追求代码优雅，引入了defer+recover机制来处理错误
 //两个都是内置函数
 
 func main() {
-test()
-fmt.Println("上面的错误正常执行。。")
-fmt.Println("开始执行下面的程序")
+	test()
+	fmt.Println("上面的错误正常执行。。")
+	fmt.Println("开始执行下面的程序")
 }
 
 func test() {
 	//利用defer和Recover机制来捕获错误
-	defer func ()  {
+	defer func() {
 		// 调用recover的内置函数，可以捕获错误
 		err := recover()
 		// 如果没有错误，返回值为零值
-		if err != nil{
+		if err != nil {
 			fmt.Println("错误已经处理")
-			fmt.Println("Err是:",err)
+			fmt.Println("Err是:", err)
 		}
 	}()
 	num1 := 10
